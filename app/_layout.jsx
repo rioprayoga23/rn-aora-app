6a676eed93fd4ed6a3725bc59ac286e31cc32c8b
Rio Prayoga <yoprayogap@gmail.com>
2024-07-21 19:24:15 +0700

    feat(auth): add sign-in screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -37,6 +37,9 @@ export default function App() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
     </Stack>
   );
 }
